4dca9c012d125c2f28e029f3f8de9d54f3ff6e50
mafhper <mafhper@gmail.com>
2021-12-06 10:31:49 -0300

    ⌨️ Back-flip-end

---
+++ code-resources/javascript/node-js/101/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+ 
+app.get('/', function (req, res) {
+  res.send('<h1 style="color: red" >Hello World!</h1>')
+})
+
+app.get('/oi', function (req, res) {
+  res.send('<h1 style="color: blue" >Olá Mundo</h1>')
+})
+
+app.listen(1234)
